5cc0fb51e7e096755402af89f3d00f6d947dcaef
baglaev <bglvssh@yandex.ru>
2023-07-11 09:37:29 +0300

    fix: fix html, css, js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const focusTimer = document.querySelector('.focus-time');
-const 
+const pomodoroButton = document.querySelector('.pomodoro-button');
+const shortBreakButton = document.querySelector('.sb-button');
+const longBreakButton = document.querySelector('.lb-button');
+const focusTime = document.querySelector('.focus-time');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+shortBreakButton.addEventListener('click', () => {
+    focusTime.classList.add('short-break');
+    focusTime.classList.remove('long-break');
+    minutes.textContent = 5;
+})
+
+longBreakButton.addEventListener('click', () => {
+    focusTime.classList.add('long-break');
+    focusTime.classList.remove('short-break');
+    minutes.textContent = 15;
+})
+
+pomodoroButton.addEventListener('click', () => {
+    focusTime.classList.remove('long-break');
+    focusTime.classList.remove('short-break');
+    minutes.textContent = 25;
+})
