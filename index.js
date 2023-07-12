5afa29f2037c48ff9bee59557db7b19224cfbbe7
baglaev <bglvssh@yandex.ru>
2023-07-12 17:08:46 +0300

    feat: hard const

--- index.js
+++ index.js
@@ -7,31 +7,38 @@ const seconds = document.querySelector('.seconds');
 const startButton = document.querySelector('.start-button');
 const stopButton = document.querySelector('.stop-button');
 const resetButton = document.querySelector('.reset-button');
+const minutesInput = document.querySelector('.input-minutes');
+const secondsInput = document.querySelector('.input-seconds');
+
+const beginMinutes = minutesInput.value = 25;
 
 shortBreakButton.addEventListener('click', () => {
     focusTime.classList.add('short-break');
     focusTime.classList.remove('long-break');
-    minutes.textContent = 5;
+    const hardMinutes = minutes.textContent = 5;
+    minutesInput.value = hardMinutes;
 })
 
 longBreakButton.addEventListener('click', () => {
     focusTime.classList.add('long-break');
     focusTime.classList.remove('short-break');
-    minutes.textContent = 15;
+    const hardMinutes = minutes.textContent = 15;
+    minutesInput.value = hardMinutes;
 })
 
 pomodoroButton.addEventListener('click', () => {
     focusTime.classList.remove('long-break');
     focusTime.classList.remove('short-break');
-    minutes.textContent = 25;
+    const hardMinutes =  minutes.textContent = 25;
+    minutesInput.value = hardMinutes;
 })
 
-startButton.addEventListener('click', () => {
-    setInterval(subtractTime, 1000)
-})
+// startButton.addEventListener('click', () => {
+//     setInterval(subtractTime, 1000)
+// })
 
-function subtractTime() {
-    if (minutes > 0) {
-        minutes.value--;
-    }
-}
+// function subtractTime() {
+//     if (minutes > 0) {
+//         minutes.value--;
+//     }
+// }
