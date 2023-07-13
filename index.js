81fe0c89cb86f9af42a16f35b0778d84cc7e9601
baglaev <bglvssh@yandex.ru>
2023-07-13 12:02:19 +0300

    1.0

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const stopButton = document.querySelector('.stop-button');
 const resetButton = document.querySelector('.reset-button');
 const setButton = document.querySelector('.set-button')
 const minutesInput = document.querySelector('.input-minutes');
-// const secondsInput = document.querySelector('.input-seconds');
+const title = document.querySelector('.title');
 
 let timer;
 let totalSeconds = 1500;
-// let initialSeconds = 1500;
-// let totalSeconds = initialSeconds;
 
 function startTimer() {
     timer = setInterval(() => {
@@ -27,7 +25,7 @@ function startTimer() {
 
       if (totalSeconds === 0) {
         clearInterval(timer);
-        alert('Время вышло, отдохните!');
+        alert('Время вышло!');
       }
     }, 1000);
 }
@@ -37,25 +35,11 @@ function stopTimer() {
 }
 
 function resetTimer() {
-    clearInterval(timer);
-    totalSeconds = 1500;
-    minutesOnDisplay.textContent = '25';
-    secondsOnDisplay.textContent = '00';
+    stopTimer();
+    focusTime.style.backgroundColor = '#BA4949';
+    title.textContent = 'Время поработать!'
+    setTimer(25);
 }
-
-// function resetTimer() {
-//     clearInterval(timer);
-//     if (!shortBreakButton.disabled && !longBreakButton.disabled) {
-//       setTimer(25);
-//     } else if (!longBreakButton.disabled && !pomodoroButton.disabled) {
-//       setTimer(15);
-//     } else {
-//       setTimer(5);
-//     }
-
-//     // return 
-// }
-
 
 function setTimer(time) {
     clearInterval(timer);
@@ -81,50 +65,19 @@ resetButton.addEventListener('click', resetTimer);
 pomodoroButton.addEventListener('click', () => {
     setTimer(25);
     focusTime.style.backgroundColor = '#BA4949';
+    title.textContent = 'Время поработать!';
 })
 
 shortBreakButton.addEventListener('click', () =>  {
     setTimer(5);
     focusTime.style.backgroundColor = '#38858A';
+    title.textContent = 'Короткий перерыв';
 });
 
 longBreakButton.addEventListener('click', () => {
     setTimer(15);
     focusTime.style.backgroundColor = '#397097';
+    title.textContent = 'Длинный перерыв';
 });
 
 setButton.addEventListener('click', setUserTimer);
-
-
-// const beginMinutes = minutesInput.value = 25;
-
-// shortBreakButton.addEventListener('click', () => {
-//     focusTime.classList.add('short-break');
-//     focusTime.classList.remove('long-break');
-//     const hardMinutes = minutes.textContent = 5;
-//     minutesInput.value = hardMinutes;
-// })
-
-// longBreakButton.addEventListener('click', () => {
-//     focusTime.classList.add('long-break');
-//     focusTime.classList.remove('short-break');
-//     const hardMinutes = minutes.textContent = 15;
-//     minutesInput.value = hardMinutes;
-// })
-
-// pomodoroButton.addEventListener('click', () => {
-//     focusTime.classList.remove('long-break');
-//     focusTime.classList.remove('short-break');
-//     const hardMinutes =  minutes.textContent = 25;
-//     minutesInput.value = hardMinutes;
-// })
-
-// startButton.addEventListener('click', () => {
-//     setInterval(subtractTime, 1000)
-// })
-
-// function subtractTime() {
-//     if (minutes > 0) {
-//         minutes.value--;
-//     }
-// }
